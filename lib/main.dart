164ba32505566017f60936155b1e24c32e4a1467
david01g <david01g@hotmail.com>
2024-02-18 20:43:20 +0100

    Fix devicepreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
